f928f00f8a304a5b7c58660436b7ac14207a3351
igortrsk <56490941+igortrsk@users.noreply.github.com>
2025-11-28 19:07:13 +0100

    OffersGrid for displaying offer component

---
+++ src/marketplacePage/components/OffersGrid.tsx
@@ -0,0 +1,20 @@
+import type { OfferType } from '../offer'
+import Offer from './Offer'
+const OffersGrid = ({ offers }: { offers: OfferType[] }) => {
+  if (!offers || offers.length === 0) {
+    return (
+      <div className="flex items-center justify-center rounded-lg border text-sm text-muted-foreground h-32">
+        <p>Brak ofert do wyświetlenia.</p>
+      </div>
+    )
+  }
+  return (
+    <div className="grid xl:grid-cols-2 sm gap-6">
+      {offers.map((offer) => (
+        <Offer key={offer.id} offer={offer} />
+      ))}
+    </div>
+  )
+}
+
+export default OffersGrid
