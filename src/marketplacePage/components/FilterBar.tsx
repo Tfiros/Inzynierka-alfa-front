86e9eb0b4fa362aa1a5f77ba8f5776f2695d18cb
igortrsk <56490941+igortrsk@users.noreply.github.com>
2025-11-28 20:20:07 +0100

    Update FilterBar.tsx

--- src/marketplacePage/components/FilterBar.tsx
+++ src/marketplacePage/components/FilterBar.tsx
@@ -12,27 +12,24 @@ import { Search } from 'lucide-react'
 
 const FilterBar = () => {
   return (
-    <section className="grid md:grid-cols-5 gap-6 items-center justify-between grid-cols-1 border-b pb-4">
+    <section className="grid md:grid-cols-5 gap-6 items-center grid-cols-1 border-b pb-4">
       <div className="md:col-span-3 items-center relative">
         <Search className="absolute ml-3 mt-3 h-4 w-4 text-muted-foreground" />
-        <Input
-          placeholder="Szukaj przedmioty..."
-          className=" pl-9 bg-gray-100"
-        />
+        <Input placeholder="Szukaj przedmioty..." className=" pl-9 bg-muted" />
       </div>
       <div className="md:col-span-2 flex flex-row gap-2 justify-between">
         <Select>
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Kategoria" />
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Gry</SelectLabel>
-                <SelectItem value="csgo">CS:GO</SelectItem>
-                <SelectItem value="valorant">Valorant</SelectItem>
-                <SelectItem value="dota2">Dota 2</SelectItem>
-              </SelectGroup>
-            </SelectContent>
           </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Gry</SelectLabel>
+              <SelectItem value="csgo">CS:GO</SelectItem>
+              <SelectItem value="valorant">Valorant</SelectItem>
+              <SelectItem value="dota2">Dota 2</SelectItem>
+            </SelectGroup>
+          </SelectContent>
         </Select>
         <Select>
           <SelectTrigger className="w-full">
