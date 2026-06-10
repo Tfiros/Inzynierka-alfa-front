2854b465d00b8e006a011c0dfd4544c7735e3c66
igortrsk <56490941+igortrsk@users.noreply.github.com>
2026-06-10 16:51:31 +0200

    remove hidden DarkModeSwitch

--- src/shared/layout/MainLayout.tsx
+++ src/shared/layout/MainLayout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom"
 import { UserNavbar } from "./navbar/views/UserNavbar"
 import { Footer } from "./Footer"
 import GuestNavbar from "./navbar/views/GuestNavbar"
-import DarkModeSwitch from "@/shared/components/DarkModeSwitch"
 import useMainLayout from "./hooks/UseMainLayout"
 import ChatWindowHost from "@/features/chat/components/ChatWindowHost"
 
@@ -15,10 +14,6 @@ const MainLayout = () => {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <div className="hidden">
-        <DarkModeSwitch />
-      </div>
-
       {isLogged ? <UserNavbar /> : <GuestNavbar />}
       <main className="flex-1 mx-auto w-full px-4 pt-6 pb-12">
         <Outlet />
